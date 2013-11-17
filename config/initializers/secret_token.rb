18f7bbc6c446e32f3db11716e8cd6bb2ac2a5cc2
Aaron Wishnick <aaronwishnick@gmail.com>
2013-11-17 12:38:17 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatDemo::Application.config.secret_key_base = '01bbe1438e59fd054e0380700c8bf4ee41d6c38d10609536be43660345342ee896c7fddd33fa5939e576c96ecc46fe735a70249e2a7abc8958f27e4ee4dfafcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChatDemo::Application.config.session_store :cookie_store, key: '_chat-demo_session'
